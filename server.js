8a1d9ed3c3ca30c1f7768771532909ab6ee936f4
anhsaigon19-cmd <anhsaigon19@gmail.com>
2026-02-03 22:50:51 +0700

    Update server.js

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const axios = require('axios');
 const app = express();
 
 // 🚨 QUAN TRỌNG: Railway cấp PORT động
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3000
 
 // Middleware
 app.use(cors());
