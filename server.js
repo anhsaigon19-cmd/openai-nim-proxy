348619787ce3d9b2f2a32cab9fb068e8c968eee4
anhsaigon19-cmd <anhsaigon19@gmail.com>
2026-02-04 12:27:55 +0700

    Update server.js

--- server.js
+++ server.js
@@ -4,7 +4,10 @@ const cors = require('cors');
 const axios = require('axios');
 
 const app = express();
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, '0.0.0.0', () => {
+  console.log(`🚀 Server running on port ${PORT}`);
+}); 
 
 // Middleware
 app.use(cors());
